0049be92a1551ff5f8a75c81cc86ff1b3d64fb8a
Jakodan007 <129838228+Jakodan007@users.noreply.github.com>
2024-01-02 17:55:23 +0000

    changed upload to .zip

--- pupil-submission-page/src/components/UploadFile.vue
+++ pupil-submission-page/src/components/UploadFile.vue
@@ -1,26 +1,36 @@
-<template>
-    <v-container>
-      <v-file-input v-model="selectedFile" label="Select a PDF file"></v-file-input>
-      <v-btn @click="uploadFile">Upload</v-btn>
-    </v-container>
-  </template>
+<script lang="ts" setup>
+import { ref } from 'vue';
 
-<script lang="ts">
-export default {
-    data() {
-      return {
-        selectedFile: null
-      };
-    },
-    methods: {
-    uploadFile() {
-      if (this.selectedFile) {
-        // Perform actions with the selected file
-        console.log('Selected File:', this.selectedFile);
-      } else {
-        console.error('No file selected');
-      }
-    }
-    }
-  };
+// Define uploadedFile ref to store the uploaded file
+const uploadedFile = ref<File | null>(null);
+
+// Modify handleFileUpload function to update the uploadedFile ref
+const handleFileUpload = (file: Event) => {
+  const uploadedZip = (file.target as HTMLInputElement).files?.[0];
+  if (uploadedZip) {
+    uploadedFile.value = uploadedZip; // Update the uploadedFile ref with the selected file
+  } else {
+    console.error('Please select a file.');
+  }
+};
+
+// Method to handle file submission
+const submitFile = () => {
+  if (uploadedFile.value) {
+    // Perform actions with the uploaded file data here
+    console.log('File submitted!');
+    console.log('Uploaded file:', uploadedFile.value);
+  } else {
+    console.error('No file uploaded.');
+  }
+};
+
 </script>
+
+<template>
+  <v-container>
+    <v-file-input accept=".zip" label="Upload Zip Folder" @input="handleFileUpload" />
+
+    <v-btn color="primary" @click="submitFile">Submit</v-btn>
+  </v-container>
+</template>
