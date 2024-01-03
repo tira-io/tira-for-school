4978f58133da8273cb9b8278e9c4254401ada1e2
Jakodan007 <129838228+Jakodan007@users.noreply.github.com>
2024-01-03 19:46:28 +0000

    created header

---
+++ pupil-submission-page/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar app>
+      <v-avatar size="150">
+        <v-img src="@/assets/uni_leipzig_logo_v2.svg" />
+      </v-avatar>
+
+      <v-spacer></v-spacer>
+      <v-btn @click="handleCenterButtonClick">Center Button</v-btn>
+      <v-spacer></v-spacer>
+
+      <v-btn @click="handleLoginButtonClick">Login</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    methods: {
+      handleCenterButtonClick() {
+        // Logic for the center button click
+      },
+      handleLoginButtonClick() {
+        // Logic for the login button click
+      },
+    },
+  };
+  </script>
--- pupil-submission-page/src/components/MainStudentPage.vue
+++ pupil-submission-page/src/components/MainStudentPage.vue
@@ -6,42 +6,47 @@ import File from "./UploadFile.vue"
 
 
 <template>
-    <v-card class="ma-6 pa-5">
-        <v-row>
-            <v-col cols="12">
-                <v-row>
-                    <!-- Zeile 1" -->
-                    <v-col cols="12">
-                        <v-card>
-                            <v-img height="300" src="@/assets/uni_leipzig_logo_v2.svg" />
-                            <v-card-title>Willkommen auf meiner Webseite</v-card-title>
-                            <v-card-text>Hier lernst du, wie man eine eigene KI trainieren kann.</v-card-text>
-                        </v-card>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <!-- Zeile zwei -->
-                    <v-col cols="12" md="6">
-                        <v-card class="flex-grow-1">
-                            <v-card-title>Übersicht Aufgaben</v-card-title>
-                            <v-card-text>
-                                Dein Auftrag, für den Fall, dass du ihn annehmen willst:
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="12" md="6">
-                        <v-card class="flex-grow-1">
-                            <v-card-title>Lade hier deine KI hoch</v-card-title>
-                            <v-card-text>
-                                hochladen - dafür musst du es erst herunterladen und abspeichern.
-                            </v-card-text>
-                            <File/>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
-    </v-card>
+    <v-row>
+        <v-col cols="12">
+            <v-row>
+                <v-col cols="12">
+                    <v-card>
+                        <p class="font-weight-black">
+                            Willkommen!
+                        </p>
+                        <p class="font-weight-medium">
+                            Auf dieser Webseite lernst du, wie man Daten für eine KI sammelt und diese selbst trainieren
+                            kann.
+                        </p>
+                        <p>
+                            Die Bilder solltest du als Hausaufgabe bereits gesammelt haben oder du lädst dir jetzt schnell
+                            welche herunter.
+                        </p>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <!-- Zeile zwei -->
+                <v-col cols="12" md="6">
+                    <v-card class="flex-grow-1">
+                        <v-card-title>Übersicht Aufgaben</v-card-title>
+                        <v-card-text>
+                            Dein Auftrag, für den Fall, dass du ihn annehmen willst:
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-card class="flex-grow-1">
+                        <v-card-title>Lade hier deine KI hoch</v-card-title>
+                        <v-card-text>
+                            hochladen - dafür musst du es erst herunterladen und abspeichern.
+                        </v-card-text>
+                        <File />
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-col>
+    </v-row>
 </template> 
 
 
