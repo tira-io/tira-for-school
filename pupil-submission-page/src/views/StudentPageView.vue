e71057d74891abf4370991f785e89a20f2cbb723
Jakodan007 <129838228+Jakodan007@users.noreply.github.com>
2024-02-13 17:11:51 +0000

    Setup Codespace deleted Header Import

--- pupil-submission-page/src/views/StudentPageView.vue
+++ pupil-submission-page/src/views/StudentPageView.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
-// import HelloWorld from "@/components/HelloWorld.vue"
-
 import StudentPage from "@/components/MainStudentPage.vue"
-import Header from "@/components/Header.vue";
 
 </script>
 
