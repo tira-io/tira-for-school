c5ca2c99e72f1d9e08b29d7718015db86b9e353f
Jakodan007 <129838228+Jakodan007@users.noreply.github.com>
2024-02-16 14:32:15 +0000

    layout stepper 2

--- pupil-submission-page/src/components/Stepper_2.vue
+++ pupil-submission-page/src/components/Stepper_2.vue
@@ -1,13 +1,31 @@
 <template>
   <v-container>
+    <v-row>
+      <v-col cols="8">
+        <ul>
+          <li class="text-left">
+            Nun musst du deinen Datensatz entsprechend labeln.
+          </li>
+          <li class="text-left">
+            Das bedeutet, dass du deine Bilder ihrer korrekten Klasse (entweder
+            "Vorfahrtsstraße" oder "Vorfahrt gewähren") zuordnen und dem
+            Computer somit die Lösung vorsagen musst, damit er mit diesen
+            Trainieren kann.
+          </li>
+          <li class="text-left">
+            Alle deine Bilder sollten dafür am besten in zwei entsprechend
+            benannten Ordnern auf deinem Computer abgespeichert sein.
+          </li>
+        </ul>
+      </v-col>
+      <v-col cols="4"> Bild einfügen </v-col>
+    </v-row>
+    <br />
     <ul>
-      <li class="text-left">Nun musst du deinen Datensatz entsprechend labeln.</li>
-      <li class="text-left">Das bedeutet, dass du deine Bilder ihrer korrekten Klasse (entweder "Vorfahrtsstraße" oder "Vorfahrt
-        gewähren") zuordnen und dem Computer somit die Lösung vorsagen musst, damit er mit diesen Trainieren kann.</li>
-      <li class="text-left">Alle deine Bilder sollten dafür am besten in zwei entsprechend benannten Ordnern
-        auf deinem Computer abgespeichert sein.</li>
-      <br>
-      <li class="text-left">Lade jetzt alle diese Bilder an der passenden Stelle hoch und klicke dann auf weiter.</li>
+      <li class="text-left">
+        Lade jetzt alle diese Bilder an der passenden Stelle hoch und klicke
+        dann auf weiter.
+      </li>
     </ul>
   </v-container>
   <upload-images-for-class title="Klasse: Vorfahrtsstra&szlig;e"
