b0b21d32e1e12814e90c9c9f113214e27a17120d
Jakodan007 <129838228+Jakodan007@users.noreply.github.com>
2024-02-16 14:11:22 +0000

    Added some explanations

--- pupil-submission-page/src/components/Stepper_1.vue
+++ pupil-submission-page/src/components/Stepper_1.vue
@@ -7,12 +7,36 @@
                 </v-row>
             </template>
         </v-img>
+
+        <v-card flat title="Willkommen auf dieser Webseite (Namen einfügen)">
+            <v-container>
+                <ul>
+                    <li>
+                        Hier kannst du lernen, wie eine Straßenschild-Klassifikation funktionieren kann.
+                    </li>
+                    <li>
+                        Um zu beginnen, brauchst du eine Datenbasis mit welcher du deine KI trainierst.
+                    </li>
+                    <li>
+                        Mache dafür mit deinen Mitschülerinnen und Mitschülern als Hausaufgabe ein paar Bilder von den zwei
+                        Straßenschilder-Arten.
+                    </li>
+                    <li>
+                        Nähere Informationen zu der <strong>Hausaufgabe</strong> findest du im Brief unseres Vorstandes.
+                    </li>
+                </ul>
+            </v-container>
+        </v-card>
+
         <div class="text-center">
-            <v-row><v-col cols="4"/><v-col cols="4"><v-btn @click="downloadMyPdf" color="blue" class="mt-5" block>Download Aufgabe</v-btn></v-col></v-row>
+            <v-row><v-col cols="4" />
+                <v-col cols="4">
+                    <v-btn @click="downloadMyPdf" color="blue" class="mt-4" block>Download
+                        Hausaufgabe</v-btn></v-col></v-row>
         </div>
     </v-card>
 </template>
-
+  
 <script lang="ts">
 import pdfPath from '@/assets/StudentAufgabe.pdf';
 
@@ -31,3 +55,4 @@ export default {
     },
 };
 </script>
+  
--- pupil-submission-page/src/components/Stepper_2.vue
+++ pupil-submission-page/src/components/Stepper_2.vue
@@ -1,27 +1,22 @@
 <template>
-    <v-container>
-        <ul>
-          <li class="text-left">Bevor du deine KI trainieren kannst, musst du einen entsprechend gelabelten Datensatz
-            vorbereitet haben.</li>
-          <li class="text-left">Du benötigst also Beispielbilder, welche ihrer korrekten Klasse (entweder
-            "Vorfahrtsstraße" oder "Vorfahrt gewähren") zuordnet sind</li>
-          <li class="text-left">Alle Bilder deines Teams sollten dafür am besten in zwei entsprechend benannten Ordnern
-            auf deinem Computer abgespeichert sein.</li>
-          <br>
-          <li class="text-left">Lade nun alle diese Bilder an der passenden Stelle hoch und klicke dann auf weiter.</li>
-        </ul>
-      </v-container>
-    <upload-images-for-class
-      title="Klasse: Vorfahrtsstra&szlig;e"
-      description='Bitte f&uuml;ge deine Bildbeispiele f&uuml;r die Klasse Vorfahrtsstra&szlig;e hinzu.'
-      :available_images='klasse_vorfahrt_strasse'
-    />
-    <br>
-    <upload-images-for-class 
-      title="Klasse: Vorfahrt Gew&auml;hren"
-      description='Bitte f&uuml;ge deine Bildbeispiele f&uuml;r die Klasse Vorfahrt Gew&auml;hren hinzu.'
-      :available_images='klasse_vorfahrt_gewaehren'
-    />
+  <v-container>
+    <ul>
+      <li class="text-left">Nun musst du deinen Datensatz entsprechend labeln.</li>
+      <li class="text-left">Das bedeutet, dass du deine Bilder ihrer korrekten Klasse (entweder "Vorfahrtsstraße" oder "Vorfahrt
+        gewähren") zuordnen und dem Computer somit die Lösung vorsagen musst, damit er mit diesen Trainieren kann.</li>
+      <li class="text-left">Alle deine Bilder sollten dafür am besten in zwei entsprechend benannten Ordnern
+        auf deinem Computer abgespeichert sein.</li>
+      <br>
+      <li class="text-left">Lade jetzt alle diese Bilder an der passenden Stelle hoch und klicke dann auf weiter.</li>
+    </ul>
+  </v-container>
+  <upload-images-for-class title="Klasse: Vorfahrtsstra&szlig;e"
+    description='Bitte f&uuml;ge deine Bildbeispiele f&uuml;r die Klasse Vorfahrtsstra&szlig;e hinzu.'
+    :available_images='klasse_vorfahrt_strasse' />
+  <br>
+  <upload-images-for-class title="Klasse: Vorfahrt Gew&auml;hren"
+    description='Bitte f&uuml;ge deine Bildbeispiele f&uuml;r die Klasse Vorfahrt Gew&auml;hren hinzu.'
+    :available_images='klasse_vorfahrt_gewaehren' />
 </template>
 
 <script lang="ts">
@@ -32,6 +27,6 @@ export default {
   data: () => ({
     cols: 2
   }),
-  components: {UploadImagesForClass},
+  components: { UploadImagesForClass },
 }
 </script>
